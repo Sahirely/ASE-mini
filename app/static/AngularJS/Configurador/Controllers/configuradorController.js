78ee38474fb96ea79847968fac9e95f88d58b190
Candi1 <aperez@bism.com.mx>
2017-05-16 17:52:53 -0500

    Front para Configurador

--- app/static/AngularJS/Configurador/Controllers/configuradorController.js
+++ app/static/AngularJS/Configurador/Controllers/configuradorController.js
@@ -7,7 +7,28 @@ registrationModule.controller('configuradorController', function ($scope, $route
 
 		$scope.show_wizard= true;
 		$scope.show_busquedaCliente=false;
+		$scope.show_cliente=true;
 
+	}
+
+	$scope.guardarCliente = function (){
+		$scope.show_cliente=false;
+		$scope.show_operacion=true;
+	}
+
+	$scope.guardarOperacion = function (){
+		$scope.show_operacion=false;
+		$scope.show_licitacion=true;
+	}
+
+	$scope.guardarLicitacion = function (){
+		$scope.show_licitacion=false;
+		$scope.show_unidad=true;
+	}
+
+	$scope.guardarUnidad = function (){
+		$scope.show_unidad=false;
+		$scope.show_modulos=true;
 	}
 });
 
