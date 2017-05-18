f28f7d03a78092721ef61027fc003c7f21dc495b
GibranQC <gibranqc9131@gmail.com>
2017-05-18 10:10:03 -0500

    ordenServicio Controller

--- app/static/AngularJS/ConsultaCitas/Repositories/consultaCitasRepository.js
+++ app/static/AngularJS/ConsultaCitas/Repositories/consultaCitasRepository.js
@@ -1,5 +1,5 @@
 var consultaCitaUrl = global_settings.urlCORS + '/api/OrdenServicio/';
-var cotizacionUrl = global_settings.urlCORS + '/api/cotizacion/';
+//var cotizacionUrl = global_settings.urlCORS + '/api/cotizacion/';
 
 registrationModule.factory('consultaCitasRepository', function($http, $q) {
     var deferred = $q.defer();
@@ -18,6 +18,71 @@ registrationModule.factory('consultaCitasRepository', function($http, $q) {
         getExisteOrden: function(idUsuario, numeroOrden) {
             return $http({
                 url: consultaCitaUrl + 'getOrdenExistente/',
+                method: "GET",
+                params: {
+                    idUsuario: idUsuario,
+                    numeroOrden: numeroOrden
+                },
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+        },
+        getOrdenAcciones: function(idUsuario, numeroOrden) {
+            return $http({
+                url: consultaCitaUrl + 'getOrdenAcciones/',
+                method: "GET",
+                params: {
+                    idUsuario: idUsuario,
+                    numeroOrden: numeroOrden
+                },
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+        },
+        getOrdenCliente: function(idUsuario, numeroOrden) {
+            return $http({
+                url: consultaCitaUrl + 'getOrdenCliente/',
+                method: "GET",
+                params: {
+                    idUsuario: idUsuario,
+                    numeroOrden: numeroOrden
+                },
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+        },
+        getOrdenDocumentos: function(idUsuario, numeroOrden) {
+            return $http({
+                url: consultaCitaUrl + 'getOrdenDocumentos/',
+                method: "GET",
+                params: {
+                    idUsuario: idUsuario,
+                    numeroOrden: numeroOrden
+                },
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+        },
+        getOrdenEvidencias: function(idUsuario, numeroOrden) {
+            return $http({
+                url: consultaCitaUrl + 'getOrdenEvidencias/',
+                method: "GET",
+                params: {
+                    idUsuario: idUsuario,
+                    numeroOrden: numeroOrden
+                },
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+        },
+        getOrdenDetalle: function(idUsuario, numeroOrden) {
+            return $http({
+                url: consultaCitaUrl + 'getOrdenDetalle/',
                 method: "GET",
                 params: {
                     idUsuario: idUsuario,
--- app/website/controllers/ordenServicio.js
+++ app/website/controllers/ordenServicio.js
@@ -30,8 +30,113 @@ OrdenServicio.prototype.get_getTotalOrdenes = function (req, res, next) {
     });
 }
 
-//Obtiene las ordenes pendientes por cobrar
+//Verifica si existe la orden de servicio
 OrdenServicio.prototype.get_getOrdenExistente = function (req, res, next) {
+    var self = this;
+    var params = [{
+        name: 'idUsuario',
+        value: req.query.idUsuario,
+        type: self.model.types.INT
+    }, {
+        name: 'numeroOrden',
+        value: req.query.numeroOrden,
+        type: self.model.types.STRING
+    }];
+
+    this.model.query('SEL_EXISTE_ORDEN_SP', params, function (error, result) {
+        self.view.expositor(res, {
+            error: error,
+            result: result
+        });
+    });
+}
+
+// Obtiene las acciones existentes por Orden
+OrdenServicio.prototype.get_getOrdenAcciones = function (req, res, next) {
+    var self = this;
+    var params = [{
+        name: 'idUsuario',
+        value: req.query.idUsuario,
+        type: self.model.types.INT
+    }, {
+        name: 'numeroOrden',
+        value: req.query.numeroOrden,
+        type: self.model.types.STRING
+    }];
+
+    this.model.query('SEL_DETALLE_ORDEN_ACCIONES_SP', params, function (error, result) {
+        self.view.expositor(res, {
+            error: error,
+            result: result
+        });
+    });
+}
+
+// Obtiene los detalles de cliente por Orden
+OrdenServicio.prototype.get_getOrdenCliente= function (req, res, next) {
+    var self = this;
+    var params = [{
+        name: 'idUsuario',
+        value: req.query.idUsuario,
+        type: self.model.types.INT
+    }, {
+        name: 'numeroOrden',
+        value: req.query.numeroOrden,
+        type: self.model.types.STRING
+    }];
+
+    this.model.query('SEL_DETALLE_ORDEN_CLIENTE_SP', params, function (error, result) {
+        self.view.expositor(res, {
+            error: error,
+            result: result
+        });
+    });
+}
+
+// Obtiene los documentos por orden
+OrdenServicio.prototype.get_getOrdenDocumentos = function (req, res, next) {
+    var self = this;
+    var params = [{
+        name: 'idUsuario',
+        value: req.query.idUsuario,
+        type: self.model.types.INT
+    }, {
+        name: 'numeroOrden',
+        value: req.query.numeroOrden,
+        type: self.model.types.STRING
+    }];
+
+    this.model.query('SEL_DETALLE_ORDEN_DOCUMENTACION_SP', params, function (error, result) {
+        self.view.expositor(res, {
+            error: error,
+            result: result
+        });
+    });
+}
+
+// Obtiene las evidencias por Orden
+OrdenServicio.prototype.get_getOrdenEvidencias = function (req, res, next) {
+    var self = this;
+    var params = [{
+        name: 'idUsuario',
+        value: req.query.idUsuario,
+        type: self.model.types.INT
+    }, {
+        name: 'numeroOrden',
+        value: req.query.numeroOrden,
+        type: self.model.types.STRING
+    }];
+
+    this.model.query('SEL_DETALLE_ORDEN_EVIDENCIA_SP', params, function (error, result) {
+        self.view.expositor(res, {
+            error: error,
+            result: result
+        });
+    });
+}
+
+// Obtien los detalles de una orden
+OrdenServicio.prototype.get_getOrdenDetalle = function (req, res, next) {
     var self = this;
     var params = [{
         name: 'idUsuario',
