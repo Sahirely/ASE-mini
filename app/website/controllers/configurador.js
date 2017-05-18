3b87cd7e932439bb19efb473f66673e42da4e3a7
Laura <laura.gordillo.vargas@gmail.com>
2017-05-18 16:14:16 -0500

    cambio configurador

--- app/website/controllers/configurador.js
+++ app/website/controllers/configurador.js
@@ -1,7 +1,7 @@
 var ConfiguradorView = require('../views/ejemploVista'),
     ConfiguradorModel = require('../models/dataAccess2');
 
-var Configurador = function (conf) {
+var Configurador = function(conf) {
     this.conf = conf || {};
 
     this.view = new ConfiguradorView();
@@ -9,13 +9,13 @@ var Configurador = function (conf) {
         parameters: this.conf.parameters
     });
 
-    this.response = function () {
+    this.response = function() {
         this[this.conf.funcionalidad](this.conf.req, this.conf.res, this.conf.next);
     }
 }
 
 //Configuraciones existentes
-Configurador.prototype.get_operaciones = function (req, res, next) { 
+Configurador.prototype.get_operaciones = function(req, res, next) {
     var self = this;
     var params = [];
 
@@ -28,7 +28,7 @@ Configurador.prototype.get_operaciones = function (req, res, next) {
 }
 
 //Inserta nueva Operación
-Configurador.prototype.post_nuevaOperacion = function (req, res, next) {
+Configurador.prototype.post_nuevaOperacion = function(req, res, next) {
     //Objeto que almacena la respuesta
     var object = {};
     //Objeto que envía los parámetros
@@ -37,68 +37,57 @@ Configurador.prototype.post_nuevaOperacion = function (req, res, next) {
     var self = this;
 
     var params = [{
-            name: 'nombreOperacion',
-            value: req.body.nombreOperacion,
-            type: self.model.types.STRING
-        },
-        {
-            name: 'nombreContacto',
-            value: req.body.nombreContacto,
-            type: self.model.types.STRING
-        },
-        {
-            name: 'correoContacto',
-            value: req.body.correoContacto,
-            type: self.model.types.STRING
-        },
-        {
-            name: 'telefonoContacto',
-            value: req.body.telefonoContacto,
-            type: self.model.types.STRING
-        },
-        {
-            name: 'fechaInicio',
-            value: req.body.fechaInicio,
-            type: self.model.types.STRING
-        },
-        {
-            name: 'fechaFin',
-            value: req.body.fechaFin,
-            type: self.model.types.STRING
-        },
-        {
-            name: 'idCatalogoTipoOperacion',
-            value: req.body.idCatalogoTipoOperacion,
-            type: self.model.types.INT
-        },
-        {
-            name: 'manejoUtilidad',
-            value: req.body.manejoUtilidad,
-            type: self.model.types.INT
-        },
-        {
-            name: 'porcentajeUtilidad',
-            value: req.body.porcentajeUtilidad,
-            type: self.model.types.DECIMAL
-        },
-        {
-            name: 'geolocalizacion',
-            value: req.body.geolocalizacion,
-            type: self.model.types.INT
-        },
-        {
-            name: 'estatusOperacion',
-            value: req.body.estatusOperacion,
-            type: self.model.types.INT
-        },
-        {
-            name: 'idCatalogoFormaPago',
-            value: req.body.idCatalogoFormaPago,
-            type: self.model.types.INT
-        }];
+        name: 'nombreOperacion',
+        value: req.body.nombreOperacion,
+        type: self.model.types.STRING
+    }, {
+        name: 'nombreContacto',
+        value: req.body.nombreContacto,
+        type: self.model.types.STRING
+    }, {
+        name: 'correoContacto',
+        value: req.body.correoContacto,
+        type: self.model.types.STRING
+    }, {
+        name: 'telefonoContacto',
+        value: req.body.telefonoContacto,
+        type: self.model.types.STRING
+    }, {
+        name: 'fechaInicio',
+        value: req.body.fechaInicio,
+        type: self.model.types.STRING
+    }, {
+        name: 'fechaFin',
+        value: req.body.fechaFin,
+        type: self.model.types.STRING
+    }, {
+        name: 'idCatalogoTipoOperacion',
+        value: req.body.idCatalogoTipoOperacion,
+        type: self.model.types.INT
+    }, {
+        name: 'manejoUtilidad',
+        value: req.body.manejoUtilidad,
+        type: self.model.types.INT
+    }, {
+        name: 'porcentajeUtilidad',
+        value: req.body.porcentajeUtilidad,
+        type: self.model.types.DECIMAL
+    }, {
+        name: 'geolocalizacion',
+        value: req.body.geolocalizacion,
+        type: self.model.types.INT
+    }, {
+        name: 'estatusOperacion',
+        value: req.body.estatusOperacion,
+        type: self.model.types.INT
+    }, {
+        name: 'idCatalogoFormaPago',
+        value: req.body.idCatalogoFormaPago,
+        type: self.model.types.INT
+    }];
 
 
-    this.model.post('INS_OPERACIONES_SP', params, function (error, result) {
+    this.model.post('INS_OPERACIONES_SP', params, function(error, result) {
         //Callback
         object.error = error;
         object.result = result;
@@ -107,4 +96,4 @@ Configurador.prototype.post_nuevaOperacion = function (req, res, next) {
     });
 }
 
-module.exports = ConfiguradorModel;
+module.exports = Configurador;
