33c42887b9f876735038cd85f21b95a1a389a07d
agrijalva <agrijalva@bism.com.mx>
2017-05-30 16:12:02 -0500

    cotizaciones
    solo se ha repetido las cotizaciones

--- app/static/AngularJS/Detalle/Controllers/detalleController.js
+++ app/static/AngularJS/Detalle/Controllers/detalleController.js
@@ -1,66 +1,66 @@
-registrationModule.controller('detalleController', function($scope, $location, consultaCitasRepository, $rootScope, $routeParams,  alertFactory, globalFactory, commonService, localStorageService, detalleRepository) {
+registrationModule.controller('detalleController', function($scope, $location, consultaCitasRepository, $rootScope, $routeParams, alertFactory, globalFactory, commonService, localStorageService, detalleRepository) {
     //*****************************************************************************************************************************//
     // $rootScope.modulo <<-- Para activar en que opción del menú se encuentra
     //*****************************************************************************************************************************//
     //$rootScope.modulo = 'reporteHistorial';
     //Inicializa la pagina
 
-    $scope.idUsuario = 2;
-    $scope.numeroOrden = $routeParams.orden;
-    $scope.textoNota = null;
-    $scope.notaTrabajo = [];
-    $scope.HistoricoOrden = [];
-    $scope.IdsCotizacionesPorOrden = [];
-    $scope.x = 0;
-    $scope.numCotz = 0;
-    $scope.HistoricoCotizaciones = [];
+    $scope.idUsuario                = 2;
+    $scope.numeroOrden              = $routeParams.orden;
+    $scope.textoNota                = null;
+    $scope.notaTrabajo              = [];
+    $scope.HistoricoOrden           = [];
+    $scope.IdsCotizacionesPorOrden  = [];
+    $scope.x                        = 0;
+    $scope.numCotz                  = 0;
+    $scope.HistoricoCotizaciones    = [];
+    $scope.CotizacionesDetalle      = [];
+
     $scope.init = function() {
         $scope.getHistoricos();
         $scope.getOrdenDetalle($scope.idUsuario, $scope.numeroOrden);
         $scope.getOrdenCliente($scope.idUsuario, $scope.numeroOrden);
         $scope.getOrdenDocumentos($scope.idUsuario, $scope.numeroOrden);
         $scope.enviaNota();
+
+        $scope.getCotizaciones( $scope.numeroOrden, 0 ); 
     };
 
-    $scope.getHistoricos = function (){
+    $scope.getHistoricos = function() {
 
-        detalleRepository.getHistoricoOrden($scope.numeroOrden).then(function(result){
-          if(result.data.length > 0){
-            $scope.HistoricoOrden = result.data;
-          }
-        },function(error){
-          alertFactory.error('No se puede obtener el historico de la orden.');
+        detalleRepository.getHistoricoOrden($scope.numeroOrden).then(function(result) {
+            if (result.data.length > 0) {
+                $scope.HistoricoOrden = result.data;
+            }
+        }, function(error) {
+            alertFactory.error('No se puede obtener el historico de la orden.');
         });
 
-        detalleRepository.getIdCotizacionesPorOrden($scope.numeroOrden).then(function(result){
+        detalleRepository.getIdCotizacionesPorOrden($scope.numeroOrden).then(function(result) {
             $scope.numCotz = result.data.length;
-            if(result.data.length > 0){
-              $scope.IdsCotizacionesPorOrden = result.data;
+            if (result.data.length > 0) {
+                $scope.IdsCotizacionesPorOrden = result.data;
             }
             $scope.getHistoricosCotz();
-        },function(error){
-          alertFactory.error('No se puede obtener las cotizaciones de la orden.');
+        }, function(error) {
+            alertFactory.error('No se puede obtener las cotizaciones de la orden.');
         });
-
-
-
-
     }
 
-    $scope.getHistoricosCotz = function(){
-        for ($scope.x = 0; $scope.x < $scope.numCotz; $scope.x ++){ 
-          detalleRepository.getHistoricoCotizacion($scope.IdsCotizacionesPorOrden[$scope.x].idCotizacion).then(function(result){
-              if(result.data.length > 0){
-                var valueToPush = {};
-                valueToPush.consecutivo = result.data[0].consecutivo;
-                valueToPush.data = result.data;
-                $scope.HistoricoCotizaciones.push(valueToPush);
-              }
-          },function(error){
-            alertFactory.error('No se pudo recuperar el historico de la cotización.');
-          });
+    $scope.getHistoricosCotz = function() {
+        for ($scope.x = 0; $scope.x < $scope.numCotz; $scope.x++) {
+            detalleRepository.getHistoricoCotizacion($scope.IdsCotizacionesPorOrden[$scope.x].idCotizacion).then(function(result) {
+                if (result.data.length > 0) {
+                    var valueToPush = {};
+                    valueToPush.consecutivo = result.data[0].consecutivo;
+                    valueToPush.data = result.data;
+                    $scope.HistoricoCotizaciones.push(valueToPush);
+                }
+            }, function(error) {
+                alertFactory.error('No se pudo recuperar el historico de la cotización.');
+            });
         }
-        console.log($scope.HistoricoCotizaciones);
+        // console.log($scope.HistoricoCotizaciones);
     }
 
 
@@ -74,7 +74,7 @@ registrationModule.controller('detalleController', function($scope, $location, c
         });
     }
 
-     $scope.getOrdenCliente = function(idUsuario, orden) {
+    $scope.getOrdenCliente = function(idUsuario, orden) {
         consultaCitasRepository.getOrdenCliente(idUsuario, orden).then(function(result) {
             if (result.data.length > 0) {
                 $scope.detalleCliente = result.data[0];
@@ -94,21 +94,34 @@ registrationModule.controller('detalleController', function($scope, $location, c
         });
     }
 
-    $scope.nuevaCotizacion = function(){
+    $scope.nuevaCotizacion = function() {
         location.href = '/cotizacionnueva?orden=' + $routeParams.orden;
     }
 
-    $scope.enviaNota = function(){
-      $scope.notaTrabajo = [];
-      var Nota = $scope.textoNota == '' ? null : $scope.textoNota;
-      detalleRepository.insNota(Nota, $scope.numeroOrden, $scope.idUsuario).then(function(result){
-          if (result.data.length > 0){
-              $scope.notaTrabajo = result.data;
-          }
-      },function(error){
-          alertFactory.error('No se pudieron obtener las notas');
-      });
-      $scope.textoNota = null;
+    $scope.enviaNota = function() {
+        $scope.notaTrabajo = [];
+        var Nota = $scope.textoNota == '' ? null : $scope.textoNota;
+        detalleRepository.insNota(Nota, $scope.numeroOrden, $scope.idUsuario).then(function(result) {
+            if (result.data.length > 0) {
+                $scope.notaTrabajo = result.data;
+            }
+        }, function(error) {
+            alertFactory.error('No se pudieron obtener las notas');
+        });
+        $scope.textoNota = null;
     };
+
+    $scope.getCotizaciones = function( numeroOrden, estatus ) {
+        // getCotizacionesByOrden: function(numeroOrden, estatus)
+        // console.log('hola mundo');
+        detalleRepository.getCotizacionesByOrden(numeroOrden, estatus).then(function(result) {
+            $scope.CotizacionesDetalle = result.data;
+            console.log( $scope.CotizacionesDetalle );
+        }, function(error) {
+            alertFactory.error('No se puede obtener los documentos de la orden');
+        });
+    }
+
+    $scope.Cotizaciones
 
 });
