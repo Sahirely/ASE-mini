5adfde30921dd46ed1366c8cf6cd79eef6643934
Edwin CRUZ <edwincrug@gmail.com>
2017-05-30 19:45:12 -0500

    POR Cobrar SQL v1

--- app/website/controllers/cobrar/sql/SEL_PORCOBRAR_SP.sql
+++ app/website/controllers/cobrar/sql/SEL_PORCOBRAR_SP.sql
@@ -1,25 +1,27 @@
-create procedure dbo.SEL_PORCOBRAR_SP (
-	@idUsuario numeric(18,0)
-)
-as
-begin
-
-	SELECT 
-		'' as cliente,
-		'' as consecutivo,
-		'' as numeroOrden,
-		'' as económico,
+ALTER PROCEDURE [dbo].[SEL_PORCOBRAR_SP] @idUsuario NUMERIC(18, 0) AS
+BEGIN
+	SELECT
+		cli.razonSocial AS cliente,
+		consecutivoOrden AS consecutivo,
+		numeroOrden AS numeroOrden,
+		uni.numeroEconomico AS economico,
+		'placas' AS placas,
 		--zonas dinámicas
-		'' as peroveedor,
-		'' as tipoOrden,
-		'' as ceco,
-		'' as provision,
-		'' as estatus,
-		'' as ventasiniva,
-		'' as fechaCita
+		'' AS proveedor,
+		tor.nombreTipoORden AS tipoOrden,
+		'' AS ceco,
+		'' AS provision,
+		'' AS estatus,
+		'' AS ventasiniva,
+		'' AS fechaCita
 	FROM
 		Ordenes ord
 	LEFT JOIN dbo.ContratoOperacion cop ON cop.idContratoOperacion = ord.idContratoOperacion
-
-end
-
+	LEFT JOIN Partidas.dbo.Contrato con ON con.idContrato = cop.idContrato
+	LEFT JOIN Partidas.dbo.Licitacion lic ON lic.idLicitacion = con.idLicitacion
+	LEFT JOIN Partidas.dbo.Cliente cli ON cli.idCliente = lic.idCliente
+	LEFT JOIN dbo.Unidades uni ON uni.idUnidad = ord.idUnidad --JOIN PROVEEDOR
+	LEFT JOIN [dbo].[CatalogoTipoOrden] tor ON tor.idTipoOrden = ord.idTipoOrden --JOIN CECOS
+	WHERE
+		ord.idOrden > 0
+	END
