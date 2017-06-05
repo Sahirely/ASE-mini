d190eff4ec5056c0da6b06f0eff2c383818a920a
GibranQC <gibranqc9131@gmail.com>
2017-06-04 21:02:37 -0500

    Cambio consulta citas

--- app/static/AngularJS/ConsultaCitas/Controllers/consultaCitasController.js
+++ app/static/AngularJS/ConsultaCitas/Controllers/consultaCitasController.js
@@ -1,9 +1,10 @@
-registrationModule.controller('consultaCitasController', function($scope, $route, $modal, $rootScope, cotizacionConsultaRepository,localStorageService, alertFactory, globalFactory, consultaCitasRepository, ordenServicioRepository, cotizacionRepository, trabajoRepository, uploadRepository) {
+registrationModule.controller('consultaCitasController', function($scope, $route, userFactory,$modal, $rootScope, cotizacionConsultaRepository,localStorageService, alertFactory, globalFactory, consultaCitasRepository, ordenServicioRepository, cotizacionRepository, trabajoRepository, uploadRepository) {
     //*****************************************************************************************************************************//
     // $rootScope.modulo <<-- Para activar en que opción del menú se encuentra
     //*****************************************************************************************************************************//
    $scope.citas = [];
-    
+    $scope.userData = userFactory.getUserData();
+    $scope.ZonasSeleccionadas = {};
     var Zona = 0//$scope.zonaSelected == '' || $scope.zonaSelected == undefined ? null : $scope.zonaSelected;
       var idEjecutivo = 0//$scope.ejecutivoSelected == '' || $scope.ejecutivoSelected == undefined ? null : $scope.ejecutivoSelected;
       var fechaMes = ''//this.obtieneFechaMes() == '' ? null : this.obtieneFechaMes();
@@ -20,6 +21,10 @@ registrationModule.controller('consultaCitasController', function($scope, $route
     
     //init de la pantalla tallerCita
     $scope.initTallerCita = function() {
+        console.log($scope.userData)
+        $scope.obtieneNivelZona();
+        $scope.devuelveEjecutivos();
+        $scope.ZonasSeleccionadas[0] = "0";
         $scope.getTotalOrdenes($scope.idContratoOperacion , tipoConsulta);
         $('#calendar .input-group.date').datepicker({
             todayBtn: "linked",
@@ -72,5 +77,154 @@ registrationModule.controller('consultaCitasController', function($scope, $route
          location.href = '/detalle?orden=' + obj.numeroOrden +'&estatus='+1;
     }
 
+      //obtiene los niveles de zona del usuario y seguidamente obtiene las zonas por nivel.
+    $scope.obtieneNivelZona = function(){
+        $scope.promise = cotizacionConsultaRepository.getNivelZona($scope.userData.contratoOperacionSeleccionada).then(function (result) {
+            $scope.totalNiveles = result.data.length;
+            if(result.data.length > 0){
+              $scope.NivelesZona = result.data;
+              $scope.devuelveZonas();
+            }
+         },
+         function (error) {
+             alertFactory.error('No se pudo ontener el nivel de zona, inténtelo más tarde.');
+         });
+    }
+
+    //obtiene las zonas por cada nivel con que cuenta el usuario
+    $scope.devuelveZonas = function() {
+      for ($scope.x = 0; $scope.x < $scope.totalNiveles; $scope.x ++){
+        cotizacionConsultaRepository.getZonas($scope.userData.contratoOperacionSeleccionada, $scope.NivelesZona[$scope.x].idNivelZona).then(function(result) {
+          if (result.data.length > 0){
+            var valueToPush = {};
+                valueToPush.orden = result.data[0].orden;
+                valueToPush.etiqueta = result.data[0].etiqueta;
+                valueToPush.data = result.data;
+            $scope.Zonas.push(valueToPush);
+            //se establece por default cada zona seleccionada en 0
+            $scope.ZonasSeleccionadas[result.data[0].orden] = "0";
+          }
+        }, function(error) {
+            alertFactory.error('No se pudo recuperar información de las zonas');
+        });
+      }
+    };
+    //realiza consulta según filtros
+    $scope.consultaCotizacionesFiltros = function() {
+      $scope.cotizaciones = [];
+      $scope.cotizacionesSinPresupuesto = [];
+      $('.ordenesPresupuesto').DataTable().destroy();
+      $('.ordenesSinPresupuesto').DataTable().destroy();
+      var Zona = $scope.zonaSelected == '' || $scope.zonaSelected == undefined ? 0 : $scope.zonaSelected;
+      var idEjecutivo = $scope.ejecutivoSelected == '' || $scope.ejecutivoSelected == undefined ? 0 : $scope.ejecutivoSelected;
+      var fechaMes = this.obtieneFechaMes();
+      var rInicio = $scope.fechaInicio == '' || $scope.fechaInicio == undefined ? '' : $scope.fechaInicio;
+      var rFin = $scope.fechaFin == '' || $scope.fechaFin == undefined ? '' : $scope.fechaFin;
+      var fecha = $scope.fecha == '' || $scope.fecha == undefined ? '' : $scope.fecha;
+      var numeroOrden = $scope.numeroTrabajo == '' || $scope.numeroTrabajo == undefined ? '' : $scope.numeroTrabajo;
+      $scope.promise = cotizacionConsultaRepository.ObtenerOrdenesTipoConsulta( $scope.userData.contratoOperacionSeleccionada, Zona, 0, idEjecutivo, fechaMes, rInicio, rFin, fecha, numeroOrden, 2).then(function (result){
+          if (result.data.length != 0){
+              $scope.cotizaciones = result.data;
+              globalFactory.filtrosTabla("ordenesPresupuesto", "Ordenes Con Presupuesto", 10);
+          }
+
+      },function (error) {
+          alertFactory.error('No se encontraron cotizaciones, inténtelo más tarde.')
+      });
+    };
+
+//obtiene los usuarios ejecutivos
+    $scope.devuelveEjecutivos = function(){
+        cotizacionConsultaRepository.obtieneEjecutivos($scope.userData.contratoOperacionSeleccionada).then(function(ejecutivos){
+            if(ejecutivos.data.length > 0){
+                $scope.listaEjecutivos = ejecutivos.data;
+            }
+        }, function(error){
+            alertFactory.error('No se pudo recuperar información de los ejecutivos');
+        });
+    };
+
+     $scope.MesChange = function (){
+        $scope.fechaInicio = '';
+        $scope.fechaFin = '';
+        $scope.fecha = '';
+    };
+
+    $scope.RangoChange = function(){
+        $scope.fechaMes = '';
+        $scope.fecha = '';
+        this.ValidaRangoFechas();
+    };
+
+    $scope.FechaChange = function(){
+        $scope.fechaMes = '';
+        $scope.fechaInicio = '';
+        $scope.fechaFin = '';
+    };
+
+    $scope.ValidaRangoFechas = function(){
+      var isValid = true;
+
+      //valida si están seleccionadas ambas fechas del rango
+      if ($scope.fechaInicio != '' && $scope.fechaFin != ''){
+          var fechaInicial = $scope.fechaInicio.split('/');
+          var fechaFinal = $scope.fechaFin.split('/');
+
+          //valida el anio
+          if(parseInt(fechaInicial[2]) > parseInt(fechaFinal[2])){
+              isValid = false;
+          }else if(parseInt(fechaInicial[2]) == parseInt(fechaFinal[2])){
+              //valida el mes
+              if(parseInt(fechaInicial[0]) > parseInt(fechaFinal[0])){
+                  isValid = false;
+              }else if(parseInt(fechaInicial[0]) == parseInt(fechaFinal[0])){
+                  //valida el día
+                  if(parseInt(fechaInicial[1]) > parseInt(fechaFinal[1])){
+                    isValid = false;
+                  }
+              }
+          }
+
+          if(isValid == false){
+              $scope.fechaInicio = '';
+              $scope.fechaFin = '';
+              alertFactory.info('La Fecha de Fin Debe Ser Posterior a la Fecha de Inicio.');
+          }
+      }
+    };
+
+    //obtiene el mes en formato de fecha
+    $scope.obtieneFechaMes = function(){
+      var result = '';
+      if ($scope.fechaMes != '' && $scope.fechaMes != null && $scope.fechaMes != undefined) {
+          var fechaPartida = $scope.fechaMes.split('-');
+          if (fechaPartida[0] == 'Enero') {
+              result = '01/01/' + fechaPartida[1];
+          } else if (fechaPartida[0] == 'Febrero') {
+              result = '01/02/' + fechaPartida[1];
+          } else if (fechaPartida[0] == 'Marzo') {
+              result = '01/03/' + fechaPartida[1];
+          } else if (fechaPartida[0] == 'Abril') {
+              result = '01/04/' + fechaPartida[1];
+          } else if (fechaPartida[0] == 'Mayo') {
+              result = '01/05/' + fechaPartida[1];
+          } else if (fechaPartida[0] == 'Junio') {
+              result = '01/06/' + fechaPartida[1];
+          } else if (fechaPartida[0] == 'Julio') {
+              result = '01/07/' + fechaPartida[1];
+          } else if (fechaPartida[0] == 'Agosto') {
+              result = '01/08/' + fechaPartida[1];
+          } else if (fechaPartida[0] == 'Septiembre') {
+              result = '01/09/' + fechaPartida[1];
+          } else if (fechaPartida[0] == 'Octubre') {
+              result = '01/10/' + fechaPartida[1];
+          } else if (fechaPartida[0] == 'Noviembre') {
+              result = '01/11/' + fechaPartida[1];
+          } else if (fechaPartida[0] == 'Diciembre') {
+              result = '01/12/' + fechaPartida[1];
+          }
+        }
+      return result;
+    }
 
 });
