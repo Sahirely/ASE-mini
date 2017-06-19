a1d43d859ea15eba6a04445b3d5dfc6beacd308d
Laura <laura.gordillo.vargas@gmail.com>
2017-06-18 23:35:27 -0500

    combos de talleres cotizacionNueva

--- app/static/AngularJS/Cotizacion/Controllers/cotizacionConsultaController.js
+++ app/static/AngularJS/Cotizacion/Controllers/cotizacionConsultaController.js
@@ -15,12 +15,13 @@ registrationModule.controller('cotizacionConsultaController', function ($scope, 
     $scope.ZonasSeleccionadas = [];
     $scope.NivelesZona = [];
     $scope.Zonas = [];
+
     $scope.estatusDashboard = 0;
 
     $scope.cotizaciones = [];
 
     $scope.init = function () {
-        //para obtener las zonas promero se inicializa la primer zona padre.
+        
         userFactory.ValidaSesion();
         $scope.userData = userFactory.getUserData();
         $scope.estatusDashboard = $routeParams.e;
@@ -28,6 +29,7 @@ registrationModule.controller('cotizacionConsultaController', function ($scope, 
           $scope.filtroEstatus = $scope.estatusDashboard;
           $scope.consultaCotizacionesFiltros();
         }
+        //para obtener las zonas promero se inicializa la primer zona padre.
         $scope.ZonasSeleccionadas[0] = "0";
         $scope.obtieneNivelZona();
         //termina el cargado de las Zonas del usuario.
