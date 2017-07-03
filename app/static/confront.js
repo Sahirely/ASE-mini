fb4db2ec161814b36e28beea0487ae89eea80ee5
swungpoet <swungpoet@hotmail.com>
2017-07-03 17:25:07 -0500

    URL

--- app/static/confront.js
+++ app/static/confront.js
@@ -1,8 +1,8 @@
 var global_settings = {
 	//urlCORS: 'http://localhost:5300',
 
-	urlCORS: 'http://189.294.141.193:5300',
-	urlDOCS: 'http://189.294.141.193:5301'
+	urlCORS: 'http://189.204.141.193:5300',
+	urlDOCS: 'http://189.204.141.193:5301'
 	//uploadPath: 'C:/Desarrollo/Flotilla/flotilla/app/uploads/'
 	//downloadPath : 'http://192.168.20.9/Documentos/'
 }
