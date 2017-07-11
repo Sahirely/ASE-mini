fd8ba4531b72b3bed532a9c92cb8b7a1119e10db
Edwin CRUZ <edwincrug@gmail.com>
2017-07-10 22:02:46 -0500

    Ultimos Ajustes Cobranza

--- app/expressServer.js
+++ app/expressServer.js
@@ -6,47 +6,7 @@ middlewares = require('./middlewares/admin'),
 router = require('./website/router'),
 multer  = require('multer');
 var path = require('path');
-/*var mkdirp = require('mkdirp');
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    var idTrabajo = req.body.idTrabajo;
-    var idCotizacion = req.body.idCotizacion;
-    if(idCotizacion == ''){
-      mkdirp(__dirname + '/static/uploads/files/' + idTrabajo, function (err) {
-           if(file.mimetype == 'image/jpeg' || file.mimetype == 'image/png' || file.mimetype == 'image/gif' 
-                || file.mimetype == 'image/jpg' || file.mimetype == 'image/bmp' || file.mimetype == 'video/mp4'){
-              mkdirp(__dirname + '/static/uploads/files/' + idTrabajo + '/' + idCotizacion + '/multimedia', function (err) {
-                cb(null, __dirname + '/static/uploads/files/' + idTrabajo + '/' + idCotizacion + '/multimedia')
-              });
-           } else{
-              mkdirp(__dirname + '/static/uploads/files/' + idTrabajo + '/documentos', function (err) {
-                  cb(null, __dirname + '/static/uploads/files/' + idTrabajo + '/documentos')
-              });
-           }
-    });
-    }else{
-      mkdirp(__dirname + '/static/uploads/files/' + idTrabajo, function (err) {
-         mkdirp(__dirname + '/static/uploads/files/' + idTrabajo + '/' + idCotizacion, function (err) {
-           if(file.mimetype == 'image/jpeg' || file.mimetype == 'image/png' || file.mimetype == 'image/gif' 
-                || file.mimetype == 'image/jpg' || file.mimetype == 'image/bmp' || file.mimetype == 'video/mp4'){
-              mkdirp(__dirname + '/static/uploads/files/' + idTrabajo + '/' + idCotizacion + '/multimedia', function (err) {
-                cb(null, __dirname + '/static/uploads/files/' + idTrabajo + '/' + idCotizacion + '/multimedia')
-              });
-           } else{
-              mkdirp(__dirname + '/static/uploads/files/' + idTrabajo + '/' + idCotizacion + '/documentos', function (err) {
-                  cb(null, __dirname + '/static/uploads/files/' + idTrabajo + '/' + idCotizacion + '/documentos')
-              });
-           }
-         });
-    }); 
-    }       
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname)
-  }
-});
 
-var upload = multer({ storage: storage })*/
 
     //Alta de opciones
     var done=false;
--- app/static/confront.js
+++ app/static/confront.js
@@ -1,8 +1,6 @@
 var global_settings = {
-	//urlCORS: 'http://localhost:5300',
-
-	urlCORS: 'http://189.204.141.193:5000',
-	urlDOCS: 'http://189.204.141.193:5001'
+	urlCORS: 'http://localhost:5300',
+	urlDOCS: 'http://localhost:5301'
 	//uploadPath: 'C:/Desarrollo/Flotilla/flotilla/app/uploads/'
 	//downloadPath : 'http://192.168.20.9/Documentos/'
 }
