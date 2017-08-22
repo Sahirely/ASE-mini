d9a19aa67c04825203250a91f43fdee75b9ec80c
swungpoet <swungpoet@hotmail.com>
2017-08-22 10:19:10 -0500

    reporte de unidades registradas

--- app/static/AngularJS/Reporte/Controllers/reporteParqueVehicularController.js
+++ app/static/AngularJS/Reporte/Controllers/reporteParqueVehicularController.js
@@ -1,106 +1,102 @@
 registrationModule.controller('reporteParqueVehicularController', function ($scope, $route, alertFactory, userFactory, configuradorRepository, reporteRepository, cotizacionConsultaRepository, globalFactory) {
+  $scope.userData = {};
+  $scope.userData = userFactory.getUserData();
+  $scope.tiposUnidad = [];
+  $scope.Unidades = [];
+  $scope.showGPS = false;
+  $scope.showCentro = false;
+  $scope.x = 0;
+  $scope.totalNiveles = 0;
+  $scope.tipoUnidadSelected = '';
+  $scope.zonaSelected = '';
+  $scope.ZonasSeleccionadas = [];
+  $scope.NivelesZona = [];
+  $scope.Zonas = [];
+  $scope.idContratoOperacion = $scope.userData.contratoOperacionSeleccionada;
+  $scope.idUsuario = $scope.userData.idUsuario;
 
-    $scope.userData = {};
-    $scope.tipoUnidadSelected = '';
-    $scope.tiposUnidad = [];
-    $scope.Unidades = [];
-    $scope.showGPS = false;
-    $scope.showCentro = false;
-    //variables para zonas
-    $scope.x = 0;
-    $scope.totalNiveles = 0;
-    $scope.zonaSelected = "0";
-    $scope.ZonasSeleccionadas = [];
-    $scope.NivelesZona = [];
-    $scope.Zonas = [];
+  $scope.init = function () {
+    userFactory.ValidaSesion();
+    $scope.GetReporteParqueVehicular();
+    $scope.ZonasSeleccionadas[0] = "0";
+    $scope.obtieneNivelZona();
+    $scope.showGPS = $scope.userData.geolocalizacion == 1 ? true : false;
+    $scope.showCentro = $scope.userData.presupuesto == 1 ? true : false;
+    if($scope.userData != null){
+      $scope.ObtenerTiposUnidad();
+    }
+  }
 
-    $scope.init = function () {
-        userFactory.ValidaSesion();
-        $scope.userData = userFactory.getUserData();
-        $scope.ZonasSeleccionadas[0] = "0";
-        $scope.obtieneNivelZona();
-        $scope.showGPS = $scope.userData.geolocalizacion == 1 ? true : false;
-        $scope.showCentro = $scope.userData.presupuesto == 1 ? true : false;
-        if($scope.userData != null){
-          $scope.ObtenerTiposUnidad();
+  $scope.obtieneNivelZona = function(){
+    $scope.promise = cotizacionConsultaRepository.getNivelZona($scope.idContratoOperacion).then(function (result) {
+      $scope.totalNiveles = result.data.length;
+      if(result.data.length > 0){
+        $scope.NivelesZona = result.data;
+        $scope.devuelveZonas();
+      }
+    },
+    function (error) {
+     alertFactory.error('No se pudo ontener el nivel de zona, inténtelo más tarde.');
+   });
+  }
+
+  //obtiene las zonas por cada nivel con que cuenta el usuario
+  $scope.devuelveZonas = function() {
+    for ($scope.x = 0; $scope.x < $scope.totalNiveles; $scope.x ++){
+      cotizacionConsultaRepository.getZonas($scope.idContratoOperacion, $scope.NivelesZona[$scope.x].idNivelZona, $scope.userData.idUsuario).then(function(result) {
+        if (result.data.length > 0){
+          var valueToPush = {};
+          valueToPush.orden = result.data[0].orden;
+          valueToPush.etiqueta = result.data[0].etiqueta;
+          valueToPush.data = result.data;
+          $scope.Zonas.push(valueToPush);
+          //se establece por default cada zona seleccionada en 0
+          $scope.ZonasSeleccionadas[result.data[0].orden] = "0";
         }
-    }
-
-    $scope.obtieneNivelZona = function(){
-        $scope.promise = cotizacionConsultaRepository.getNivelZona($scope.userData.contratoOperacionSeleccionada).then(function (result) {
-            $scope.totalNiveles = result.data.length;
-            if(result.data.length > 0){
-              $scope.NivelesZona = result.data;
-              $scope.devuelveZonas();
-            }
-         },
-         function (error) {
-             alertFactory.error('No se pudo ontener el nivel de zona, inténtelo más tarde.');
-         });
-    }
-
-    //obtiene las zonas por cada nivel con que cuenta el usuario
-    $scope.devuelveZonas = function() {
-      for ($scope.x = 0; $scope.x < $scope.totalNiveles; $scope.x ++){
-        cotizacionConsultaRepository.getZonas($scope.userData.contratoOperacionSeleccionada, $scope.NivelesZona[$scope.x].idNivelZona, $scope.userData.idUsuario).then(function(result) {
-          if (result.data.length > 0){
-            var valueToPush = {};
-                valueToPush.orden = result.data[0].orden;
-                valueToPush.etiqueta = result.data[0].etiqueta;
-                valueToPush.data = result.data;
-            $scope.Zonas.push(valueToPush);
-            //se establece por default cada zona seleccionada en 0
-            $scope.ZonasSeleccionadas[result.data[0].orden] = "0";
-          }
-        }, function(error) {
-            alertFactory.error('No se pudo recuperar información de las zonas');
-        });
-      }
-    };
-
-    $scope.cambioZona = function(id, orden){
-      //al cambiar de zona se establece como zona seleccionada.
-      $scope.zonaSelected = id;
-      //se limpian los combos siguientes.
-      for($scope.x = orden+1; $scope.x <= $scope.totalNiveles; $scope.x ++){
-        $scope.ZonasSeleccionadas[$scope.x] = "0";
-      }
-    }
-
-    $scope.ObtenerTiposUnidad = function () {
-        var idOp = $scope.userData.idOperacion;
-        configuradorRepository.getTipoUnidades(3).then(function(result){
-            if(result.data.length > 0){
-              $scope.tiposUnidad = result.data;
-            }else{
-              alertFactory.info('La Operación no Cuenta con Tipos de Unidad Configurados.');
-            }
-        }, function (error) {
-            alertFactory.error('Ocurrio un Error al Cargar los Tipos de Unidad.');
-        });
-    }
-
-    $scope.GetReporteParqueVehicular = function(){
-      var idOp = $scope.userData.idOperacion;
-      var filtro = $scope.tipoUnidadSelected == '' || $scope.tipoUnidadSelected == undefined ? null :  $scope.tipoUnidadSelected;
-      var idZona = $scope.zonaSelected == undefined || $scope.zonaSelected == '' ? null : $scope.zonaSelected;
-      reporteRepository.reporteParqueVehicular(3, filtro, idZona).then(function(result){
-        
-          if(result.data.length > 0){
-              $scope.Unidades = result.data;
-              globalFactory.filtrosTabla("parqueVehicular","Parque Vehicular",5);
-          }else {
-              $scope.Unidades = [];
-              globalFactory.filtrosTabla("parqueVehicular","Parque Vehicular",5);
-              alertFactory.info('No se Encontraron Unidades');
-          }
-      },function (error){
-          alertFactory.error('Ocurrio un Error al Cargar el Reporte.');
+      }, function(error) {
+        alertFactory.error('No se pudo recuperar información de las zonas');
       });
     }
+  };
 
-    $scope.irDetalle = function(numEco){
-     
-      location.href = '/unidad?economico='+numEco;
+  $scope.cambioZona = function(id, orden){
+    $scope.zonaSelected = id;
+    for($scope.x = orden+1; $scope.x <= $scope.totalNiveles; $scope.x ++){
+      $scope.ZonasSeleccionadas[$scope.x] = "0";
     }
+  }
+
+  $scope.ObtenerTiposUnidad = function () {
+    reporteRepository.getTipoUnidades($scope.idContratoOperacion).then(function(result){
+      if(result.data.length > 0){
+        $scope.tiposUnidad = result.data;
+      }else{
+        alertFactory.info('La Operación no Cuenta con Tipos de Unidad Configurados.');
+      }
+    }, function (error) {
+      alertFactory.error('Ocurrio un Error al Cargar los Tipos de Unidad.');
+    });
+  }
+
+  $scope.GetReporteParqueVehicular = function(){
+    $scope.Unidades = [];
+    $('.parqueVehicular').DataTable().destroy();
+    var idTipoUnidad = $scope.tipoUnidadSelected == '' || $scope.tipoUnidadSelected == undefined ? null :  $scope.tipoUnidadSelected;
+    var idZona = $scope.zonaSelected == '' || $scope.zonaSelected == '0' || $scope.zonaSelected == undefined ? null : $scope.zonaSelected;
+    reporteRepository.reporteParqueVehicular($scope.idContratoOperacion, idTipoUnidad, idZona).then(function(result){      
+      if(result.data.length > 0){
+        $scope.Unidades = result.data;
+        globalFactory.filtrosTabla("parqueVehicular", "Parque Vehicular", 100);
+        alertFactory.success('Unidades encontradas.');
+      }else { 
+        alertFactory.info('No se encontraron unidades');
+      }
+    },function (error){
+      alertFactory.error('Ocurrio un Error al Cargar el Reporte.');
+    });
+  }
+
+  $scope.irDetalle = function(numEco){
+    location.href = '/unidad?economico='+numEco;
+  }
 });
--- app/static/AngularJS/Reporte/Repositories/reporteRepository.js
+++ app/static/AngularJS/Reporte/Repositories/reporteRepository.js
@@ -2,69 +2,31 @@ var reporteGralUrl = global_settings.urlCORS + '/api/reporte/';
 
 registrationModule.factory('reporteRepository', function ($http) {
     return {
-          reporteParqueVehicular: function(idOperacion, filtro, idZona){
-            return $http({
-                   url: reporteGralUrl + 'ReporteParqueVehicular',
-                   method: "GET",
-                   params: {
-                       idOperacion: idOperacion,
-                       filtro: filtro,
-                       idZona: idZona
-                   },
-                   headers: {
-                       'Content-Type': 'application/json'
-                   }
-               });
-          }
-        // reporteGral: function () {
-        //     return $http({
-        //         url: reporteGralUrl + 'reportegral',
-        //         method: "GET"
-        //     });
-        // },
-        //
-        // reporteAntiguedad: function (fechaInicio, fechaFin, zona, tar, estatus, numeroTrabajo, tipo, idUsuario, tipofecha) {
-        //     return $http({
-        //         url: reporteGralUrl + 'reporteAntiguedad',
-        //         method: "GET",
-        //         params: {
-        //             fechaInicio: fechaInicio,
-        //             fechaFin: fechaFin,
-        //             zona: zona,
-        //             tar: tar,
-        //             estatus: estatus,
-        //             numeroTrabajo: numeroTrabajo,
-        //             tipo: tipo,
-        //             idUsuario: idUsuario,
-        //             tipofecha:tipofecha
-        //         },
-        //         headers: {
-        //             'Content-Type': 'application/json'
-        //         }
-        //     });
-        // },
-        //
-        // estatus: function () {
-        //     return $http({
-        //         url: reporteGralUrl + 'estatus',
-        //         method: "GET",
-        //         headers: {
-        //             'Content-Type': 'application/json'
-        //         }
-        //     });
-        // },
-        //
-        // callcenter: function (idUsuario) {
-        //     return $http({
-        //         url: reporteGralUrl + 'callcenter',
-        //         method: "GET",
-        //         params: {
-        //             idUsuario: idUsuario
-        //         },
-        //         headers: {
-        //             'Content-Type': 'application/json'
-        //         }
-        //     });
-        // }
-    };
+      reporteParqueVehicular: function(idOperacion, idTipoUnidad, idZona){
+        return $http({
+         url: reporteGralUrl + 'ReporteParqueVehicular',
+         method: "GET",
+         params: {
+             idOperacion: idOperacion,
+             idTipoUnidad: idTipoUnidad,
+             idZona: idZona
+         },
+         headers: {
+             'Content-Type': 'application/json'
+         }
+     });
+    },
+    getTipoUnidades: function(idOperacion){
+        return $http({
+         url: reporteGralUrl + 'tipoUnidad',
+         method: "GET",
+         params: {
+             idOperacion: idOperacion
+         },
+         headers: {
+             'Content-Type': 'application/json'
+         }
+     });
+    }
+};
 });
