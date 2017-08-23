5380e221ca7c17d7752e3cc24a36765490b8ba5c
Sahirely Yam <syam@bism.com.mx>
2017-08-22 19:05:56 -0500

    reporte utilidad

--- app/static/AngularJS/Reporte/Repositories/reporteUtilidadRepository.js
+++ app/static/AngularJS/Reporte/Repositories/reporteUtilidadRepository.js
@@ -1,23 +1,25 @@
 var reporteUtilidadUrl = global_settings.urlCORS + '/api/reporte/';
 
 registrationModule.factory('reporteUtilidadRepository', function ($http) {
-    return {        
-        getUtilidad: function (fechaInicio, fechaFin, fechaMes, rangoInicial, rangoFinal, zona, tar, idTipoCita, estatus, numeroTrabajo, bandera) {
+    return {
+        getUtilidad: function (idOperacion, numOrden){//fechaInicio, fechaFin, fechaMes, rangoInicial, rangoFinal, zona, tar, idTipoCita, estatus, numeroTrabajo, bandera) {
             return $http({
                 url: reporteUtilidadUrl + 'reporteUtilidad/',
                 method: "GET",
                 params: {
-                    fechaInicio:fechaInicio,
-                    fechaFin:fechaFin,
-                    fechaMes:fechaMes,
-                    rangoInicial:rangoInicial,
-                    rangoFinal:rangoFinal,
-                    zona:zona,
-                    tar:tar,
-                    idTipoCita:idTipoCita,
-                    estatus:estatus,
-                    numeroTrabajo:numeroTrabajo,
-                    bandera:bandera
+                    idOperacion: idOperacion,
+                    numOrden:numOrden
+                    // fechaInicio:fechaInicio,
+                    // fechaFin:fechaFin,
+                    // fechaMes:fechaMes,
+                    // rangoInicial:rangoInicial,
+                    // rangoFinal:rangoFinal,
+                    // zona:zona,
+                    // tar:tar,
+                    // idTipoCita:idTipoCita,
+                    // estatus:estatus,
+                    //
+                    // bandera:bandera
                 },
                 headers: {
                     'Content-Type': 'application/json'
