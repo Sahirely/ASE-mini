0d7eb1a7908a68656d50c9ea9482df4fe9e0b396
Jorge Fernandez <jfernandezsistemas@gmail.com>
2017-09-04 16:24:22 -0500

    Revert "Merge branch 'master' of https://github.com/swungpoet/ASEv2"
    This reverts commit ccbb9a746b0e168ab982ef58e912ecb73eca3c90, reversing
    changes made to 23bf2c9ee951d313bea3e72ad076d61e2cb060c0.

--- app/static/AngularJS/Reporte/Repositories/reporteUtilidadRepository.js
+++ app/static/AngularJS/Reporte/Repositories/reporteUtilidadRepository.js
@@ -2,7 +2,7 @@ var reporteUtilidadUrl = global_settings.urlCORS + '/api/reporte/';
 
 registrationModule.factory('reporteUtilidadRepository', function ($http) {
     return {
-        getUtilidad: function (tipoConsulta, idOperacion, numOrden, fechaInicial, fechaFinal, fechaMes, idZona, rangoInicial, rangoFinal, idTipoOrden, idEstatus, isProduction){//fechaInicio, fechaFin, fechaMes, rangoInicial, rangoFinal, zona, tar, idTipoCita, estatus, numeroTrabajo, bandera) {
+        getUtilidad: function (tipoConsulta, idOperacion, numOrden, fechaInicial, fechaFinal, fechaMes, idZona, rangoInicial, rangoFinal, idTipoOrden, idEstatus){//fechaInicio, fechaFin, fechaMes, rangoInicial, rangoFinal, zona, tar, idTipoCita, estatus, numeroTrabajo, bandera) {
             return $http({
                 url: reporteUtilidadUrl + 'reporteUtilidad/',
                 method: "GET",
@@ -17,8 +17,7 @@ registrationModule.factory('reporteUtilidadRepository', function ($http) {
                     rangoInicial : rangoInicial,
                     rangoFinal : rangoFinal,
                     idTipoOrden : idTipoOrden,
-                    idEstatus : idEstatus,
-                    isProduction: isProduction
+                    idEstatus : idEstatus
                 },
                 headers: {
                     'Content-Type': 'application/json'
--- app/website/controllers/reporte.js
+++ app/website/controllers/reporte.js
@@ -206,11 +206,6 @@ Reporte.prototype.get_reporteUtilidad = function(req, res, next) {
             name: 'idEstatus',
             value: req.query.idEstatus,
             type: self.model.types.INT
-        },
-        {
-            name: 'isProduction',
-            value: req.query.isProduction,
-            type: self.model.types.INT
         }
     ];
 
