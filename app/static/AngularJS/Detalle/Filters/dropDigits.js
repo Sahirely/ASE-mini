f47d3376369e876ef5fbf1e02c3f11be4ca6d88a
Jorge Fernandez <jfernandezsistemas@gmail.com>
2017-10-24 11:02:43 -0500

    Changes to dropDigits filter, also working with currency now

--- app/static/AngularJS/Detalle/Filters/dropDigits.js
+++ app/static/AngularJS/Detalle/Filters/dropDigits.js
@@ -1,11 +1,12 @@
-var dropDigits = function() {
-    return function(floatNum) {
+var dropDigits = function($filter) {
+    return function(floatNum, symbol) {
 
-        return String(floatNum)
+        var value = String(floatNum)
             .split('.')
             .map(function(d, i) { return i ? d.substr(0, 2) : d; })
             .join('.');
 
+        return $filter('currency')(value, symbol, 2);
     };
 
 };
