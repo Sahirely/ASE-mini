bdb6224f503dbbb90ac8d377d25c4f2e93a94183
Edwin CRUZ <edwincrug@gmail.com>
2017-12-07 17:19:02 -0600

    reporte Timeline

--- app/static/confront.js
+++ app/static/confront.js
@@ -1,7 +1,7 @@
 var global_settings = {
-	//urlCORS: 'http://localhost:5100',
-	//urlDOCS: 'http://localhost:5101'
-	urlCORS: 'http://189.204.141.193:5100',
-	urlDOCS: 'http://189.204.141.193:5101'
+	urlCORS: 'http://localhost:5100',
+	urlDOCS: 'http://localhost:5101'
+	// urlCORS: 'http://189.204.141.193:5100',
+	// urlDOCS: 'http://189.204.141.193:5101'
 	
 }
